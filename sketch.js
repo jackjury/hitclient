edac1a1c10028f49bd0b5adfba93e0715e07dc1a
Jack Jury <jackjury@Jacks-MacBook-Pro.local>
2021-01-24 17:33:36 +0000

    initial commit of api based client

---
+++ sketch.js
@@ -0,0 +1,37 @@
+let width = 500;
+let height = 72;
+let count;
+let digits = 7;
+let counturl = "http://localhost:5000/count";
+
+function preload() {
+  count = loadJSON(counturl);
+}
+
+function setup() {
+  count = count.count.toString();
+  console.log(count);
+  count = count.split("").map((x) => parseInt(x));
+  while (count.length < digits) {
+    let zero = 0;
+    count.unshift(zero);
+  }
+  console.log(count);
+  let canvas = createCanvas(width, height);
+  //  background(255);
+  drawDigits();
+}
+function drawDigits() {
+  let x = 0;
+  let y = 0;
+  let w = width / digits - 5;
+  textAlign(CENTER, CENTER);
+  textSize(height);
+  for (let i = 0; i < digits; i++) {
+    fill(0);
+    rect(x, y, w, height);
+    fill(0, 255, 0);
+    text(count[i], x + w / 2, height / 2);
+    x += w + 5;
+  }
+}
